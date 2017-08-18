6b12c64a9670efa7237abfbc9034760e35a6065c
Eli Kellendonk <eli@pixeltribe.com>
2017-08-18 14:13:33 -0600

    move componenet file structure, and add components to app

--- src/app/shopping-list/shopping-edit/shopping-edit.component.spec.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ShoppingListEditComponent } from './shopping-list-edit.component';
+import { ShoppingListEditComponent } from './shopping-edit.component';
 
 describe('ShoppingListEditComponent', () => {
   let component: ShoppingListEditComponent;
