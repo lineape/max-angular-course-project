8a8b0717edf7d31c8cf5577548e0ab765350085f
Eli Kellendonk <eli@pixeltribe.com>
2017-08-23 17:59:30 -0600

    Add ingredients to shopping list using viewChild (To be replaced with forms in the future)

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingListEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInput: ElementRef;
+  @ViewChild('amountInput') amountInput: ElementRef;
+  @Output() addToShoppingList = new EventEmitter<Ingredient>();
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onAdd(e: Event) {
+    e.preventDefault();
+    const name = this.nameInput.nativeElement.value;
+    const amount = parseInt(this.amountInput.nativeElement.value, 10);
+
+    const nameIsLongEnough = name.trim().length > 2;
+    const amountIsLargerThan0 = !isNaN(amount) && amount > 0;
+    if (nameIsLongEnough && amountIsLargerThan0) {
+      this.addToShoppingList.emit(new Ingredient(name, amount));
+    }
   }
-
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 
+const isSameIngredient = a => b => a.name === b.name;
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient[] = [ new Ingredient('Cheese', 3) ];
+  ingredients: Ingredient[] = [new Ingredient('Cheese', 3)];
 
   constructor() {}
 
   ngOnInit() {}
+
+  onAddToShoppingList(ingredient: Ingredient) {
+    if (this.ingredientIsInList(ingredient)) {
+      return this.mergeIngredients(ingredient);
+    }
+    this.ingredients.push(ingredient);
+    this.sortIngredients();
+  }
+
+  mergeIngredients(ingredient: Ingredient) {
+    this.ingredients.find(isSameIngredient(ingredient)).amount +=
+      ingredient.amount;
+  }
+
+  ingredientIsInList(ingredient: Ingredient) {
+    return this.ingredients.some(isSameIngredient(ingredient));
+  }
+  sortIngredients() {
+    this.ingredients.sort(
+      (a, b) => (a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1),
+    );
+  }
 }
