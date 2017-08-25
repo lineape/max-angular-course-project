4585812b07d8c9f496747c54a0f667f88cf5a09d
Eli Kellendonk <eli@pixeltribe.com>
2017-08-24 18:09:59 -0600

    add ingredients to recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,9 +9,13 @@ import { RecipeService } from '../recipe.service';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [];
+
   constructor(private recipeService: RecipeService) {}
 
   ngOnInit() {
     this.recipes = this.recipeService.getRecipes();
+    this.recipeService.listChanged.subscribe(newList => {
+      this.recipes = newList;
+    });
   }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,22 +1,49 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
-  private recipes: Recipe[] = [
+  private list: Recipe[] = [
     new Recipe(
-      'Recipe 1',
-      'Delicious',
+      'Pepperoni Pizza',
+      'A delicious pepperoni pizza, from scratch!',
       'https://cdn.pixabay.com/photo/2016/03/05/19/24/cheese-1238395_960_720.jpg',
+      [
+        new Ingredient('Cheese', 3),
+        new Ingredient('Tomato sauce', 1),
+        new Ingredient('Pepperoni', 5),
+        new Ingredient('Flour', 2),
+        new Ingredient('Eggs', 2),
+      ],
     ),
     new Recipe(
-      'Cheese2',
-      'Delicious',
+      'Chocolate Cake',
+      'Amazing chocolate cake.',
       'https://cdn.pixabay.com/photo/2016/03/05/19/24/cheese-1238395_960_720.jpg',
+      [
+        new Ingredient('Flour', 4),
+        new Ingredient('Eggs', 3),
+        new Ingredient('Sugar', 5),
+        new Ingredient('Unsweetened Chocolate', 2),
+      ],
     ),
   ];
   recipeSelected = new EventEmitter<Recipe>();
+  listChanged = new EventEmitter<Recipe[]>();
+
+  addRecipe(recipe: Recipe) {
+    this.list.push(recipe);
+    this.sortRecipes();
+    this.listChanged.emit(this.getRecipes());
+  }
+
+  private sortRecipes() {
+    this.list.sort(
+      (a, b) => (a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1),
+    );
+  }
 
   getRecipes(): Recipe[] {
-    return [...this.recipes];
+    return [...this.list];
   }
 }
