970ff1b0c7f40f3d72824d0af27d5ad6a5801d9b
Eli Kellendonk <eli@pixeltribe.com>
2017-08-28 11:34:43 -0600

    remove custom navigation from header.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,20 +1,8 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
-  @Output() navigateTo = new EventEmitter<string>();
-  showDropdown = false;
-
-  onNavigateTo(link: string) {
-    this.navigateTo.emit(link);
-  }
-
-  onToggleDropdown(e: Event) {
-    e.preventDefault();
-    this.showDropdown = !this.showDropdown;
-  }
-}
+export class HeaderComponent {}
