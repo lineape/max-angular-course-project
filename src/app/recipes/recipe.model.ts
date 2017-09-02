c5c40a4e15571da719c57b27163f1d9019145417
Eli Kellendonk <eli@pixeltribe.com>
2017-09-01 18:11:40 -0600

    Add form validation to Recipe model

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,4 +1,5 @@
 import { Ingredient } from '../shared/ingredient.model';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 export class Recipe {
   constructor(
@@ -8,5 +9,15 @@ export class Recipe {
     public ingredients: Ingredient[],
   ) {
     this.ingredients.sort(Ingredient.sortNameAsc);
+  }
+
+  static getForm() {
+    const { required, minLength } = Validators;
+    return new FormGroup({
+      name: new FormControl(null, [required, minLength(2)]),
+      description: new FormControl(null, [required, minLength(5)]),
+      imagePath: new FormControl(null, [required, minLength(5)]),
+      ingredients: new FormArray([]),
+    });
   }
 }
