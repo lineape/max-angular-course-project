7067e5ee793056449f9355a90823044e8397b462
Eli Kellendonk <eli@pixeltribe.com>
2017-09-28 12:57:53 -0600

    implement CanLoad on guards so that lazy-loaded modules can be checked with canLoad

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -1,18 +1,26 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { CanActivate, CanLoad, Router } from '@angular/router';
 
 import { AuthService } from './auth.service';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
   constructor(private authService: AuthService, private router: Router) {}
 
-  async canActivate(route, state): Promise<boolean> {
-    if (!await this.authService.isAuthenticated()) {
+  canActivate(route, state): Promise<boolean> {
+    return this.isNotAuthenticated();
+  }
+
+  canLoad(): Promise<boolean> {
+    return this.isNotAuthenticated();
+  }
+
+  private async isNotAuthenticated(): Promise<boolean> {
+    if (await this.authService.isAuthenticated()) {
+      return true;
+    } else {
       this.router.navigate(['/auth/signin']);
       return false;
-    } else {
-      return true;
     }
   }
 }
--- src/app/auth/not-auth-guard.service.ts
+++ src/app/auth/not-auth-guard.service.ts
@@ -1,13 +1,21 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { CanActivate, CanLoad, Router } from '@angular/router';
 
 import { AuthService } from './auth.service';
 
 @Injectable()
-export class NotAuthGuard implements CanActivate {
+export class NotAuthGuard implements CanActivate, CanLoad {
   constructor(private authService: AuthService, private router: Router) {}
 
-  async canActivate(route, state): Promise<boolean> {
+  canActivate(route, state): Promise<boolean> {
+    return this.isNotAuthenticated();
+  }
+
+  canLoad(): Promise<boolean> {
+    return this.isNotAuthenticated();
+  }
+
+  private async isNotAuthenticated(): Promise<boolean> {
     if (await this.authService.isAuthenticated()) {
       this.router.navigate(['/']);
       return false;
