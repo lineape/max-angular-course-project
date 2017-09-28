d8902eccba8a63a00c6e3976d7eef65ac4c2bb16
Eli Kellendonk <eli@pixeltribe.com>
2017-09-28 13:00:25 -0600

    remove uneeded SharedModule import from AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   bootstrap: [AppComponent],
   declarations: [AppComponent],
-  imports: [
-    AppRoutingModule,
-    BrowserModule,
-    CoreModule,
-    HttpModule,
-    SharedModule,
-  ],
+  imports: [AppRoutingModule, BrowserModule, CoreModule, HttpModule],
 })
 export class AppModule {}
